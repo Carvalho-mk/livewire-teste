f30f6b750907b76a5306aa5b157e963986495a77
Maycon Carvalho <m.karvalhoo@gamil.com>
2021-08-06 10:01:11 -0300

    primeiro comit

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name= "viewport" content="width=device-width, initial-scale=1.0">  
+<title>Page Title</title>
+@livewireStyles
+</head>
+<body>
+<div class='container'>
+{{$slot}}
+</div>
+
+
+@livewireScripts    
+</body>
+</html>
